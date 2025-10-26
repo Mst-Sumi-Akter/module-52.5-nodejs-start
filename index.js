a1154058765f66fb037701f1cb33f2e1a154f336
Sumi Akter <sumiwdcse@gmail.com>
2025-10-27 01:53:19 +0600

    practice module 52.1

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+const port = 5000
+
+app.get('/', (req, res)=>
+{
+    res.send('Hello from my  first ever server');
+
+})
+app.get('/about', (req, res)=>
+{
+    res.send('Hello from my  about server');
+
+})
+app.listen(port,()=>{
+    console.log(`my first is running on port:${port}`)
+})
